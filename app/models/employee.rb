5523f8558d346f06ef6b1416b74e765bfbbe33a3
VictorRMA <victor.rma@gmail.com>
2017-02-12 13:18:33 -0200

    add employee validations

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,12 @@
 class Employee < ActiveRecord::Base
+  validates :first_name, :last_name,
+            presence: true,
+            length: { minimum: 2 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@fusionltda.com\z/i
+  validates :email,
+            presence: true,
+            length: { maximum: 105 },
+            uniqueness: { case_sensitive: false },
+            format: { with: VALID_EMAIL_REGEX }
 
 end
