972dc73079fd90e9442740adc82efdbbf891c7ea
VictorRMA <victor.rma@gmail.com>
2017-04-09 15:32:53 -0300

    Integration test and some implementations on cost_center

--- app/controllers/cost_centers_controller.rb
+++ app/controllers/cost_centers_controller.rb
@@ -1,11 +1,20 @@
 class CostCentersController < ApplicationController
 
   def index
-
+    @cost_centers = CostCenter.all
   end
 
   def new
+    @cost_center = CostCenter.new
+  end
 
+  def create
+    @cost_center = CostCenter.new(cost_center_params)
+    if @cost_center.save
+      redirect_to cost_centers_path
+    else
+      render 'new'
+    end
   end
 
   def show
@@ -16,4 +25,8 @@ class CostCentersController < ApplicationController
 
   end
 
+  private
+    def cost_center_params
+      params.require(:cost_center).permit(:number, :name)
+    end
 end
---
+++ test/integration/create_cost_centers_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class CreateCostCentersTest < ActionDispatch::IntegrationTest
+
+  test "get new cost center form and create cost center" do
+    get new_cost_center_path
+    assert_template 'cost_centers/new'
+    assert_difference 'CostCenter.count', 1 do
+      post '/cost_centers', params: {cost_center: {number: 1, name: "Cost Center test name"}}
+      follow_redirect!
+    end
+    assert_template 'cost_centers/index'
+    assert_match "Cost Center test name", response.body
+  end
+end
