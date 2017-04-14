f758c7841d0d65bbe1b1a6fd53a0f96572090eb2
VictorRMA <victor.rma@gmail.com>
2017-04-14 13:39:36 -0300

    Fix uniqueness of number for costCenter

--- app/models/cost_center.rb
+++ app/models/cost_center.rb
@@ -1,5 +1,6 @@
 class CostCenter < ActiveRecord::Base
+  validates :number, presence: true
   validates :name, presence: true,
                    length: { in: 3..32 }
-  validates_uniqueness_of :name
+  validates_uniqueness_of :number
 end
--- test/models/cost_center_test.rb
+++ test/models/cost_center_test.rb
@@ -15,12 +15,6 @@ class CostCenterTest < ActiveSupport::TestCase
     assert_not @cost_center.valid?
   end
 
-  test "name should be unique" do
-    @cost_center.save
-    cost_center2 = CostCenter.new(number: 2, name: "Cost Center test name")
-    assert_not cost_center2.valid?
-  end
-
   test "name should not be too long" do
     @cost_center.name = "a" * 33;
     assert_not @cost_center.valid?
@@ -29,6 +23,17 @@ class CostCenterTest < ActiveSupport::TestCase
   test "name should not be too short" do
     @cost_center.name = "aa"
     assert_not @cost_center.valid?
+  end
+
+  test "number should be present" do
+    @cost_center.number = ""
+    assert_not @cost_center.valid?
+  end
+
+  test "number should be unique" do
+    @cost_center.save
+    cost_center2 = CostCenter.new(number: 1, name: "Another Cost Center test name")
+    assert_not cost_center2.valid?
   end
 
 end
