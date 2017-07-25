bc541d3210b03db97bd33afd71abb5badb70860c
VictorRMA <victor.rma@gmail.com>
2017-07-24 22:40:16 -0300

    Add date interval verification

--- app/models/work_time.rb
+++ app/models/work_time.rb
@@ -1,4 +1,10 @@
 class WorkTime < ActiveRecord::Base
   belongs_to :employee
   belongs_to :task
+
+  validate :check_dates
+
+  def check_dates
+    errors.add(:base, "End date should be greater than start") if self.datetime_start >= self.datetime_end
+  end
 end
